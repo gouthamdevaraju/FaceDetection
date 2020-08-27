dfd887c05720b73c7082f66d8891ded6aecae51f
gouthamdevaraju <gouthamdevaraju@gmail.com>
2020-08-27 10:39:27 +0530

    Added Xcode project, Added GitIgnore file.

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Goutham Devaraju on 27/08/20.
+//  Copyright © 2020 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
